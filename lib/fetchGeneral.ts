77179c04ae15b1b1d4f18df16316303dd58bc4fe
Febre van der Merwe <febrevdmerwe@gmail.com>
2026-04-10 08:50:50 +0200

    Add contactUsContent to fetchGeneral function

--- lib/fetchGeneral.ts
+++ lib/fetchGeneral.ts
@@ -1,11 +1,12 @@
 import { sanityClient } from "./sanity";
 
 export const fetchGeneral = async () => {
-    const data = await sanityClient.fetch(`*[_type == 'generalSettings'][0]{
+  const data = await sanityClient.fetch(`*[_type == 'generalSettings'][0]{
     phone,
     email,
     socials,
-    rules
+    rules,
+    contactUsContent
   }`);
-    return data;
+  return data;
 };
